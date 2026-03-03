02ac149f56f17889d7bc6e79dd94355b352bacdb
Belen Canalda <belencanalda5@gmail.com>
2026-03-02 21:50:40 -0300

    feat implementar middleware de autenticación

---
+++ src/middleware/Auth.ts
@@ -0,0 +1,40 @@
+import type { Request, Response, NextFunction } from 'express'
+import jwt from "jsonwebtoken";
+import Usuario from '../models/Usuario';
+
+declare global {
+    namespace Express {
+        interface Request {
+            user?: Usuario
+        }
+    }
+}
+
+export const authenticate = async (req: Request, res: Response, next: NextFunction) => {
+    const bearer = req.headers.authorization
+
+        if(!bearer){
+            const error = new Error('No autorizado')
+            return res.status(401).json({msg: error.message})
+        }
+
+        const [ , token ] = bearer.split(' ')
+
+        if(!token){
+            const error = new Error ('No autorizado')
+            return res.status(401).json({msg: error.message})
+        }
+
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+            
+            if(typeof decoded === 'object' && decoded.id ){
+                req.user = await Usuario.findByPk(decoded.id, {
+                    attributes: ['id', 'nombre', 'email']
+                })
+                next()
+            }
+        } catch (error) {
+            res.status(500).json({msg: 'Token no válido'})
+        }
+}
